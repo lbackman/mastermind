70866fd7321e90f2d866bba5638792be849d451c
Lucas Bäckman <lucas.backman@gmail.com>
2022-05-11 11:21:02 +0300

    Add methods for checking guesses with repeats
    These methods will give an accurate answer regardless of repeating
    digits in the answer key.

--- mastermind.rb
+++ mastermind.rb
@@ -27,3 +27,41 @@ end
 def sorted_guesses(guess, key)
   guess_accuracy(guess, key).sort.reverse
 end
+
+def check_exact(guess, key)
+  final = []
+  i = 0
+  while i < guess.size
+    if key[i] == guess[i]
+      final << 2
+      key.delete_at(i)
+      guess.delete_at(i)
+    else
+      i += 1
+    end
+  end
+  final
+end
+
+def check_inexact(guess, key, final)
+  guess.each_index do |i|
+    if key.include?(guess[-(i + 1)])
+      final << 1
+      key.delete_at(key.index(guess[-(i + 1)]))
+    end
+  end
+  final
+end
+
+def append_zeros(final)
+  (4 - final.size).times { final << 0 }
+  final
+end
+
+def check_guesses(guess, key)
+  temp_guess = guess[0, 4]
+  temp_key = key[0, 4]
+  partial = check_exact(temp_guess, temp_key)
+  final = check_inexact(temp_guess, temp_key, partial)
+  append_zeros(final)
+end
